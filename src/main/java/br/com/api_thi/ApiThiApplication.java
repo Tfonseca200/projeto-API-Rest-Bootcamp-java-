fd1e38dd57d646cceb6c913ae3a6b8a06ee0bd17
Thiago <thiagofonsecaclaudino@gmail.com>
2024-07-21 22:40:24 -0300

    arquitertura inicial

---
+++ src/main/java/br/com/api_thi/ApiThiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_thi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiThiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiThiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api_thi/ApiThiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_thi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiThiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
