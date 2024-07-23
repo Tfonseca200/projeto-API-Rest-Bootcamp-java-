1fa0891edea8f75b8af87179899a8c229f7ee59c
Thiago <thiagofonsecaclaudino@gmail.com>
2024-07-23 01:49:08 -0300

    adicionando exceções http

---
+++ src/main/java/br/com/api_thi/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package br.com.api_thi.controller.exception;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBussinessException (IllegalArgumentException bussinessException){
+        return new ResponseEntity<>(bussinessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException (NoSuchElementException notFoundException){
+        return new ResponseEntity<>("Resource ID not found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpecteException (Throwable unexpecteException){
+        var message = "Unexpected server error, see the logs";
+        logger.error(message, unexpecteException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
